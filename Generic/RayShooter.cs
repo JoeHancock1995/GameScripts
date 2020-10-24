44c2d0a335e226ad15752ea566a3dc5cc6cc06a3
Joseph <joe.hancock95@gmail.com>
2020-10-24 00:23:01 -0500

    added AI code

--- Generic/RayShooter.cs
+++ Generic/RayShooter.cs
@@ -1,42 +1,46 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class RayShooter : MonoBehaviour {
-    private Camera _camera;
+	private Camera _camera;
 
-    void start() {
-        _camera = GetComponent<Camera>();
+	void Start() {
+		_camera = GetComponent<Camera>();
 
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-    void OnGUI() {
-        int size = 12;
-        float posX = _camera.pixelWidth/2 - size/4;
-        float posY = +camera.pixelHeight/2 - size/2;
-        GUI.Label(new Rect(posX, posY, size, size), "*");
-    }
-    void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
-            Ray ray = _camera.ScreenPointToRay(point);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)) {
-                GameObject hitObject = hit.transform.gameObject;
-                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
-                if (target ! = null) {
-                    target.ReactToHit();
-                } else {
-                StartCoroutine(SphereIndicator(hit.point));
-            }
-        }
-    }
-    private IEnumerator SphereIndicator(Vector3 pos) {
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.sphere);
-        sphere.transform.position = pos;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
 
-        yield return new WaitForSeconds(1);
+	void OnGUI() {
+		int size = 12;
+		float posX = _camera.pixelWidth/2 - size/4;
+		float posY = _camera.pixelHeight/2 - size/2;
+		GUI.Label(new Rect(posX, posY, size, size), "*");
+	}
 
-        Destroy(sphere); 
-    }
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
+			Ray ray = _camera.ScreenPointToRay(point);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				GameObject hitObject = hit.transform.gameObject;
+				ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+				if (target != null) {
+					target.ReactToHit();
+				} else {
+					StartCoroutine(SphereIndicator(hit.point));
+				}
+			}
+		}
+	}
+
+	private IEnumerator SphereIndicator(Vector3 pos) {
+		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphere.transform.position = pos;
+
+		yield return new WaitForSeconds(1);
+
+		Destroy(sphere);
+	}
 }
